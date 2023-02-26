83bc7f53c6541487a9498a2164478f672b20ded3
MEHMETTOKGOZ <42mt06@gmail.com>
2023-02-26 12:29:22 +0300

    shareStory button active

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -3,9 +3,29 @@ import { Box } from "@mui/system";
 import homeImage from "../assets/homeImage1.jpg";
 import "./home.scss";
 import { Button, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState,useEffect } from "react";
 
 function Home() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate= useNavigate();
+  const userId = localStorage.getItem("userId");
+
+  useEffect(() => {
+    if (userId) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+function shareStory () {
+  isLoggedIn ? (
+    navigate("/add")
+  ):(navigate("/login"))
+}
+
   return (
     <Box className="main-box">
       <img className="homeimage" src={homeImage} alt="#" />
@@ -14,7 +34,7 @@ function Home() {
           Share Your Diaries
         </Typography>
         <Box className="button-box">
-          <Button>Share Story</Button>
+          <Button onClick={shareStory}>Share Story</Button>
           <Button className= "diary-button" 
           LinkComponent={Link} to = "/diaries">View Diaries</Button>
         </Box>
